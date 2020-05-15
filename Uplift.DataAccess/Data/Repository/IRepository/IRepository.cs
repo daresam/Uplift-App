c0f8994906641146248c3b4c0aeda29f97c5c0a6
Dare Sam <dawakesam@gmail.com>
2020-05-15 23:58:56 +0100

    Organizing code in tiers

---
+++ Uplift.DataAccess/Data/Repository/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Uplift.DataAccess.Data.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+       T Get(int id);
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includedProperties = null
+            );
+    }
+}
