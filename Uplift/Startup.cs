e4c5bb2cc8f8c50f677b650e2569705b0d7d4035
Dare Sam <dawakesam@gmail.com>
2020-06-04 00:01:03 +0100

    Frequency CRUD completed

--- Uplift/Startup.cs
+++ Uplift/Startup.cs
@@ -33,7 +33,7 @@ namespace Uplift
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
+            services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
